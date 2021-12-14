21a5f301e96266e491063a1447332a5d5beb7b1c
cosminrotariu <cosmin.rotariu27@gmail.com>
2021-12-14 17:55:53 +0200

    added comms, changed theme, everything is done

--- gui.py
+++ gui.py
@@ -1,16 +1,15 @@
 import PySimpleGUI as sg
 import main
 
-sg.theme("Black")
+sg.theme("DarkBlue")
 
-available_currencies = main.get_available_currencies()
-available_currencies.append('RON')
-available_currencies.sort()
+available_currencies = main.get_available_currencies()  # se obtin toate monedele disponibile pe site
+available_currencies.sort()  # se sorteaza alfabetic
 
 layout = [
     [sg.Text("Input currency")],
     [sg.Combo(available_currencies, size=(6, 6), key='input_currency', default_value='EUR', readonly=True),
-     sg.Input('1', key='input', size=(30, 1), focus=True, enable_events=True)],
+     sg.Input('1', key='input', size=(30, 1), enable_events=True)],
     [sg.Text("Output currency")],
     [sg.Combo(available_currencies, size=(6, 6), key='output_currency', default_value='RON', readonly=True),
      sg.Input('', key='output', size=(30, 1), readonly=True, disabled_readonly_background_color='black')],
@@ -23,11 +22,12 @@ window = sg.Window('Currency converter', layout)
 
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'exit':
+    if event == sg.WIN_CLOSED or event == 'exit':  # daca se apasa pe exit se va inchide fereastra
         break
-    elif event == 'input' and values['input'] != '' and values['input'][-1] not in '.0123456789':
+    elif event == 'input' and values['input'] != '' and values['input'][-1] \
+            not in '.0123456789':  # daca se introduc valori in afara de cifre si . se va afisa un mesaj
         window['output'].update("Please write the amount in digits.")
         window['input'].update('')
-    elif event == 'convert':
+    elif event == 'convert':  # daca se apasa pe convert, se face conversia
         window['output'].update(main.convert(values['input_currency'], values['input'], values['output_currency']))
 window.close()
